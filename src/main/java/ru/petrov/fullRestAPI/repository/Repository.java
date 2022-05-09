56570fcc25c81e6d452fcc6c9490eb75f3dadfe0
RedArmyIsStrongestAll <redarmyisstrongestall@yandex.ru>
2022-05-09 11:42:37 +0300

    Add interface Repository

--- src/main/java/ru/petrov/fullRestAPI/repository/Repository.java
+++ src/main/java/ru/petrov/fullRestAPI/repository/Repository.java
@@ -1,2 +1,12 @@
-package ru.petrov.fullRestAPI.repository;public interface Repository {
+package ru.petrov.fullRestAPI.repository;
+
+import ru.petrov.fullRestAPI.model.Book;
+import java.util.List;
+
+public interface Repository {
+    void saveBook(long id, Book obj);
+    boolean deleteBook(long id);
+    boolean changeBook(long id, Object param);
+    List<Book> returnAll ();
+    Book returnOne (long id);
 }
