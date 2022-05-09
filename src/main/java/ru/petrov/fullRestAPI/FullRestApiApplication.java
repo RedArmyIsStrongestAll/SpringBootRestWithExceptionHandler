8f3b0dfc5181239189ae58c031e803355d14397a
RedArmyIsStrongestAll <redarmyisstrongestall@yandex.ru>
2022-05-09 11:02:02 +0300

    Begining: create project

---
+++ src/main/java/ru/petrov/fullRestAPI/FullRestApiApplication.java
@@ -0,0 +1,13 @@
+package ru.petrov.fullRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullRestApiApplication.class, args);
+	}
+
+}
